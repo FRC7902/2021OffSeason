a8d56fab567a5470d8ef52a48275e4550458106e
MasterSeafood <yellowwatermelon14@gmail.com>
2021-04-09 18:01:59 -0400

    Added multiple files

---
+++ LearnJava/src/App.java
@@ -0,0 +1,35 @@
+
+//kjadksadjlsajd
+
+
+/*
+lkajdlsakld
+
+
+aslidjaslskjdad
+
+
+oiasjdoisajdsad
+
+*/
+
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+
+        String string = "Hello";
+
+
+        System.out.println(string.charAt(1));
+
+
+
+
+
+
+
+
+
+    }
+}
