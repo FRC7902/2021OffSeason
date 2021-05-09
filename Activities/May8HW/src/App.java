fb1a71f318c3eb7eb2cc1bf7422625de38094699
MasterSeafood <43724836+MasterSeafood@users.noreply.github.com>
2021-05-09 11:54:04 -0400

    Added May8HW

---
+++ Activities/May8HW/src/App.java
@@ -0,0 +1,90 @@
+/*
+Follow the instructions 
+Don't search things up
+If you are unsure about your answer, leave it blank. We'll discuss it later.
+
+This HW is about
+- declaration and initialization
+- operations 
+- booleans
+- chars
+- Strings
+- Math functions
+
+*/
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        //Q1 In your own words, describe what is happening on each line 
+        int x; //Ans:
+        x = 4; //Ans:
+
+        //Which one is the declaration and which one is the initilization?
+        
+
+        //Q2 create 2 booleans(one that is true, another that is false)
+
+        
+
+
+        //Using those variables along with the 3 boolean operators, print out the value true 4 times each in a different way. 
+        //System.out.println(true); NOT ALLOWED
+        //Make sure you use each of the 3 boolean operators at least once among your 4 print statements. (you don't have to use all 3 every time) 
+        //You don't need to use both of the variables in every line 
+
+
+
+
+
+        //Q3 Create 2 integer variables. (assign them any value)
+        
+
+
+
+        //Perform and print each math operation on them (+, -, *, /, %)
+        //Make sure that it is printed as a double value (type casting)
+
+
+        //Using the 2 integers along with the comparison operators (>, <, ==, <=, >=, !=), write the value true in 6 ways
+        //You can use math operators such as x + 2 > y 
+
+
+
+        //Q4 Make a string (assign it anything)
+
+
+        //print the length of the string
+
+
+        //Q5 Remember those 2 ints you made in question 3?
+        //Print the greater one (using the Math. functions);
+
+        //Print the lesser one
+
+
+        //Print the square root of one of the them (it can be a decimal)
+
+        
+        //Subtract 999 from one of the variables in 2 different ways
+
+
+        //Find the absolute value of that variable
+
+
+
+        /* 
+
+        What will be the two outputs of this: (hint: the first is a boolean, the second is a integer)
+        int x = 4;
+        System.out.println(x++>4);
+        System.out.println(x);
+        //Ans:
+        
+        
+        */
+
+        
+        //Yay you are done
+    }
+}
