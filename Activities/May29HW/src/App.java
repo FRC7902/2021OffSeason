13bb974db4786fcb1cda6cebc6bb11fe5495948b
parivansjalia <74889163+parivansjalia@users.noreply.github.com>
2021-06-05 15:33:50 -0400

    HW may 29

--- Activities/May29HW/src/App.java
+++ Activities/May29HW/src/App.java
@@ -26,30 +26,44 @@ public class App {
     }
 
         //Q1 Make a method that prints out "Hello" 3 times. 
-
+        public static void hi {
+            System.out.println ("Hello");
+            System.out.println ("Hello");
+            System.out.println ("Hello");
+        }
 
 
         //Q2 Make a method that takes in an integer and prints out the integer less 2. 
-
+        public static void hey (int a) { 
+            System.out.println (a -= 2);
+        }
 
 
 
         //Q3 Make a method that takes in a string and prints out the string 3 times.
-
+        public static void hello (String a) { 
+            System.out.println (a);
+            System.out.println (a);
+            System.out.println (a);
+        }
 
 
         //Q4 Make a method that takes in an integer and returns the integer less 2. 
-
+        public static void hey (int a) { 
+            return a -= 2;
+        }
 
         //Q5 Make a method that takes in and prints out an array of Strings
+        public static void hey (String []) { 
+            System.out.println (String []);
+        }
 
-
-        //Q6 Make a variable that any method can access 
-
+        //Q6 Make a variable that any method can access
+        //not sure how to do
 
 
         //Q7 True or False, you can have methods with the same name.
-        //Ans:
+        //Ans: yes
 
 
         //BONUS Make a method that takes in an array of integers and returns the sum of all the integers. 
