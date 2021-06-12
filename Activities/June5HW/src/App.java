dd71f9ed8a49ca68c49d38360341ae9f1a5f331b
parivansjalia <74889163+parivansjalia@users.noreply.github.com>
2021-06-12 16:49:49 -0400

    hw june 5

--- Activities/June5HW/src/App.java
+++ Activities/June5HW/src/App.java
@@ -19,25 +19,47 @@ public class App {
          //Q1 Make a function that takes in 2 numbers and returns their difference (make sure the difference is always positive)
          public static int (int a, int b) {
              int a - int b = int c;
-             return c;
+             return (Math.abs(c));
          }
 
          //Q2 Make a function that takes in 2 integers and returns an array of integers from the lowest number to the highest number
          // e.g Input: (2, 5)
          // Output {2, 3, 4, 5}
-         public static int [] (int [] numm) {
-             
+         public static int [] (int nums [] ) {
+           for (i=0;i<highestnumberinarray;i++) {
+            if (nums [i] + 1 != 0) {
+                nums [i+2]; 
+                nums [i+2] = nums [i+1];
+                nums [i+1] = nums [i] + 1;
+           }
+           }  
+            return nums;
          }
 
          //Q3 Make a function that takes in an array of integers and 1 number. The function should return a boolean array of whether each number is greater than the input number
          //e.g Input: ({3, 4, 1}, 2)
          // Output {true, true, false}
-
+         public static boolean (int xyz[], int c) {
+             boolean [] yesno = new boolean [xyz.length];
+             for (i = 0; i < xyz.length; i++) {
+                 if (xyz[i] > c) {
+                     yesno [i] = true;
+                 }
+                 else {
+                     yesno [i] = false;
+                 } 
+             }
+             return yesno;
+         }
 
 
          //Q4 Make a function that takes in an array of integers and returns the product of all the numbers in the integer
          //e.g Input: {1, 2, 3, 4, 5}
          //Output: 120
+         public static int (int [] iii) {
+             
+            return y;
+         }
 
          //BONUS Make a function that takes in an array of integers and returns the number of unique numbers in the array
          //e.g Input : {1, 2, 3, 2, 1}
@@ -49,6 +71,4 @@ public class App {
 
     }
 
-
-     
 }
