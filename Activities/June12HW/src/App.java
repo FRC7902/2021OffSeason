9cfd05e867cd51e6aaf7ba392d67e1e5c81d6e10
MasterSeafood <43724836+MasterSeafood@users.noreply.github.com>
2021-06-13 18:20:45 -0400

    Added June12HW

---
+++ Activities/June12HW/src/App.java
@@ -0,0 +1,36 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        /**
+         * 
+         * June12HW
+         * 
+         * More review
+         * 
+         * Complete the questions, push when done.
+         * 
+         * Also try and get that red squiggly error line to work again
+         * 
+         * Enjoy :)
+         * 
+         * 
+         */
+
+
+         
+    }
+
+    //Q1 Make a function that takes in a number N and returns the number of multiples of 10 between 1 and N (assume N is positive)
+
+
+    //Q2 Make a function that takes in 2 numbers N and M and returns the number of multiples of M between 1 and N (assume they're both positive)
+
+    //Q3 Make a function that takes in a number N and returns an array of multiples of 10 between 1 and N (assume N is positive again)
+
+
+    //Q4 Make a function that takes in an array of prices and returns the total cost (total cost = sum of prices * 1.13 tax)
+
+    //Q5 Make a function that checks if a number is prime (a number that is not prime will have a divisor that is not 1 or itself)
+
+    //Q6 Make up your own function that does something cool. 
+
+}
--- Activities/May29HW/src/App.java
+++ Activities/May29HW/src/App.java
@@ -1,4 +1,6 @@
 public class App {
+
+    public static int x = 0;
     public static void main(String[] args) throws Exception {
         /*
             May 29 HW
@@ -18,15 +20,16 @@ public class App {
         */
 
 
+        int[] arr = {3, 4, 2, 4, 1};
 
+        System.out.println(name(arr));
         
-
         
 
     }
 
         //Q1 Make a method that prints out "Hello" 3 times. 
-        public static void hi {
+        public static void hi (){
             System.out.println ("Hello");
             System.out.println ("Hello");
             System.out.println ("Hello");
@@ -49,13 +52,15 @@ public class App {
 
 
         //Q4 Make a method that takes in an integer and returns the integer less 2. 
-        public static void hey (int a) { 
-            return a -= 2;
+        public static int hey2 (int a) { 
+            return a-2;
         }
 
         //Q5 Make a method that takes in and prints out an array of Strings
-        public static void hey (String []) { 
-            System.out.println (String []);
+        public static void hey (String[] array) { 
+            for(String item : array){
+                System.out.println(item);
+            }
         }
 
         //Q6 Make a variable that any method can access
@@ -67,22 +72,24 @@ public class App {
 
 
         //BONUS Make a method that takes in an array of integers and returns the sum of all the integers. 
-        public static int (int [] abcd) {
-            for (i = 0; i < abcd.length; i++) {
-                int ee = abcd[i] + ee;
+        public static int method1(int [] abcd) {
+            int ee = 0;
+            for (int i = 0; i < abcd.length; i++) {
+                
+                ee = abcd[i] + ee;
             }
             return ee;
         }
 
         //BONUS2 Make a method that takes in an array of integers and returns the greatest integer.
-        public static int (int [] azy) {
-            for (ii = 0; ii < azy.length; ii++) {
-                for (iii= ii + 1; iii < azy.length - 1; iii++) {
-                    if (azy [ii] > azy [iii]) {
-                        iii=ii;
-                    }
+        public static int name(int[] azy) {
+            int bigI = 0;
+            for (int i = 1; i < azy.length; i++) {
+                if(azy[bigI] < azy[i]){
+                    bigI = i;
                 }
-                    return iii;
+            }
+            return azy[bigI];
         }
 
 
