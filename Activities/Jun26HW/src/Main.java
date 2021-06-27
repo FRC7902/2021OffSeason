16c7a557766872610fea08bbbc3dd768d331b80e
MasterSeafood <43724836+MasterSeafood@users.noreply.github.com>
2021-06-27 12:50:21 -0400

    Added June26HW

---
+++ Activities/Jun26HW/src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        /**
+         * June26HW
+         * 
+         * All about Classes
+         */
+
+
+         //Q1 Create a class Car
+
+
+         //Q2 Give the class Car a couple of attributes (number of wheels, colour, etc) 
+         //Make sure they're non-static
+
+
+         //Q3 In the main method, create an object of Car
+
+         //Q4 Print out that object's attributes
+
+         //Q5 Change one of the attributes and print it out again. 
+
+         //Q6 Add a static attribute to the Car class (maybe a boolean called hasDriver)
+
+         //Q7 Use the previous object to print out the static attribute
+
+         //Q8 Use the class itself to print out the static attribute (don't use the object)
+
+
+         //All done!
+    }
+}
