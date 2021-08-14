1f9f724675c932499eb065c69917407d69d9b7c3
parivansjalia <74889163+parivansjalia@users.noreply.github.com>
2021-08-14 16:48:09 -0400

    HW

--- Activities/Aug8HW/src/App.java
+++ Activities/Aug8HW/src/App.java
@@ -1,5 +1,24 @@
+import newObject.cat;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        
+
+        tree tree1 = new tree(8, 101);
+        System.out.println(tree1.height);
+        System.out.println(tree1.type);
+
+        tree1.height = 9;
+        tree1.introduction();
+        tree1.IncreaseHeight();
+
+        System.out.println(tree1.getAge());
+        System.out.println(tree1.setAge(99));
+
+
+        cat cat1 = new cat();
     }
-}
+
+    public array[] rectangle (int a, int b, int c, int d) {
+
+    }
+    } 
---
+++ Activities/Aug8HW/src/newObject/cat.java
@@ -0,0 +1,7 @@
+package newObject;
+
+public class cat {
+    int height = 8;
+    int age = 9;
+    String breed = "tabby cat";
+}
