1b04479b5c5fe13f5fd00698025944213e5b5cd1
Victoria <68347113+Victoria-DR@users.noreply.github.com>
2021-09-29 09:52:32 -0400

    Add gigabyte delivery

--- 2021Research/src/main/java/frc/robot/commands/DeliverGigabytes.java
+++ 2021Research/src/main/java/frc/robot/commands/DeliverGigabytes.java
@@ -1,5 +1,33 @@
 package frc.robot.commands;
 
-public class DeliverGigabytes {
-    
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.IntakeSubsystem;
+public class DeliverGigabytes extends CommandBase {
+    private final IntakeSubsystem m_intakeSubsystem;
+
+    public DeliverGigabytes(IntakeSubsystem intakeSubsystem) {
+        m_intakeSubsystem = intakeSubsystem;
+
+        addRequirements(intakeSubsystem);
+    }
+
+    @Override
+    public void execute() {
+        m_intakeSubsystem.release();
+    }
+
+    @Override
+    public void initialize() {
+        m_intakeSubsystem.stop();
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        m_intakeSubsystem.stop();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
 }
